bed8ef39ba2ee6fe8b6924d5031c5b5384130ccf
editornank <>
2023-06-05 12:16:43 +0300

    change ./py2bin.sh

--- py2bin.sh
+++ py2bin.sh
@@ -1,9 +1,7 @@
 #!/bin/env bash
-[ -z "$@" ] && echo "Use: $0 <file.py>" && exit 1
+[ -z "$1" -o  "--help" == "$1" ] && echo "Use: $0 <file.py>" && exit 1
 
 docker=podman
 docker_image=docker.io/editorbank/py2bin
-docker_container=py2bin
 
-$docker ps -q -a -f name=$docker_container | xargs -r $docker rm -f
-$docker run -it --rm -v $PWD:/pwd --name $docker_container $docker_image /compile.sh $@
+$docker run -it --rm -v $PWD:/pwd --name py2bin-$RANDOM $docker_image /compile.sh $@
